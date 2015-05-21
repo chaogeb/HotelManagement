003a27aac9fec33f95d351ccfcf3aaddb51b656e
Chaoge Zheng <procg@outlook.com>
2015-05-21 23:46:19 +0800

    Add NewReservation - CustomerDetails

--- HotelManagementSystem/MainWindow.xaml.cs
+++ HotelManagementSystem/MainWindow.xaml.cs
@@ -15,13 +15,64 @@ using System.Windows.Shapes;
 namespace HotelManagementSystem
 {
     /// <summary>
+    /// Made by Chaoge Zheng
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Boolean registerEnabled;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        // NewReservationTab - CustomerDetailsTab
+        // 预订 - 旅客信息
+        private void GoToReceiptBtn_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        { }
+
+        private void BackToAvailibiltyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BookingTab.SelectedIndex = 0;
+        }
+
+        private void CheckBoxRegister_Checked(object sender, RoutedEventArgs e)
+        {
+            CustomerDetailsSearchBtn.IsEnabled = false;
+            ClearCustomerDetails();
+            EnableDisabletextBoxes(true);
+            registerEnabled = true;
+        }
+
+        private void ClearCustomerDetails()
+        {
+            CustomerDetailsEmailTbx.Text = "";
+            CustomerDetailsFirstNameTbx.Text = "";
+            CustomerDetailsLastnameTbx.Text = "";
+            CustomerDetailsCreditCardNoTbx.Text = "";
+            CustomerDetailsPhoneCountryCodeTbx.Text = "";
+            CustomerDetailsPhoneNoTbx.Text = "";
+        }
+
+        private void CheckBoxRegister_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CustomerDetailsSearchBtn.IsEnabled = true;
+            EnableDisabletextBoxes(false);
+            registerEnabled = false;
+        }
+
+        private void EnableDisabletextBoxes(bool enabled)
+        {
+            CustomerDetailsFirstNameTbx.IsEnabled = enabled;
+            CustomerDetailsLastnameTbx.IsEnabled = enabled;
+            CustomerDetailsCreditCardNoTbx.IsEnabled = enabled;
+            CustomerDetailsPhoneCountryCodeTbx.IsEnabled = enabled;
+            CustomerDetailsPhoneNoTbx.IsEnabled = enabled;
+        }
+
     }
 }
