0552c2965d0f6a30ae41d3ae6282f19eccbe971e
Chaoge Zheng <procg@outlook.com>
2015-05-31 18:16:05 +0800

    Use SQLite with .Net Framework 4.5.1

---
+++ ControllerLayer/SQLiteController.cs
@@ -0,0 +1,65 @@
+﻿using Interface;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+
+/// <summary>
+/// Require C:\Program Files (x86)\System.Data.SQLite\2013\bin\System.Data.SQLite.dll
+/// Version: SQLite for .NET Framework 4.5.1
+/// Download from http://system.data.sqlite.org/
+/// or copy: Project_Dir\..\ControllerLayer\System.Data.SQLite.dll
+/// </summary>
+namespace ControllerLayer
+{
+    internal class SQLiteController
+    {
+        #region Misc
+
+        SQLiteConnection sqlCon = null;
+
+        /// <summary>
+        /// Opens the connection to the database.
+        /// </summary>
+        /// <returns>If success.</returns>
+        private void connect()
+        {
+            try
+            {
+                sqlCon.Open();
+            }
+            catch
+            {
+                throw new Exception("Connection to database failed!");
+            }
+        }
+
+        /// <summary>
+        /// Closes the connection to the database.
+        /// </summary>
+        /// <returns>Id success.</returns>
+        private void disconnect()
+        {
+            try
+            {
+                sqlCon.Close();
+            }
+            catch
+            {
+                throw new Exception("Disconnection from database failed!");
+            }
+        }
+
+        internal void DatabaseController(string user, string pass)
+        {
+            string dbPath = "Data Source =" + Environment.CurrentDirectory + "/HotelDataBase.db";
+            sqlCon = new SQLiteConnection(dbPath);
+        }
+
+        #endregion
+
+
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,21 @@
 School project using C#. BUPT.
 
 A WPF gui of hotel management system.
+
+Developed with Visual Studio 2013 & [SQLite][1].
+
+View SQLite database with [DB Browser for SQLite][2]
+
+## Download link:
+
+[System.Data.SQLite][3]
+
+[DB Browser for SQLite][4] (win64)
+
+[1]: http://system.data.sqlite.org/
+
+[2]: http://sqlitebrowser.org/
+
+[3]: http://system.data.sqlite.org/downloads/1.0.97.0/sqlite-netFx451-setup-bundle-x86-2013-1.0.97.0.exe " sqlite-netFx451-setup-bundle-x86-2013-1.0.97.0.exe "
+
+[4]: https://github.com/sqlitebrowser/sqlitebrowser/releases/download/v3.6.0/sqlitebrowser-3.6.0v3-win64.exe " sqlitebrowser-3.6.0v3-win64.exe "
