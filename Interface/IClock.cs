be8824bf1716d221cfe17e3937452249f209d85e
Chaoge Zheng <procg@outlook.com>
2015-06-05 23:37:55 +0800

    Add Environment Clock

---
+++ Interface/IClock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Interface
+{
+    // Made by chaogeb
+    public interface IClock
+    {
+        DateTime Time           { get; set; }   // present date & time
+        string GetReservCount   { get; }        // get reservations id
+        string GetBookingCount  { get; }        // get bookings id
+        string GetCustomerCount { get; }        // get customers id
+        string GetRoomCount     { get; }        // get rooms id
+    }
+}
