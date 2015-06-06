937285fa2406f077d931f14df8dc2a4d961fb5d7
strongman1995 <none954@163.com>
2015-06-06 10:32:16 +0800

    initialize

---
+++ Model/Initialize.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interface;
+using Model;
+
+
+namespace Model
+{
+    class Initialize
+    {
+        public Room RoomInitialize()
+        {
+            string id=Clock.GetRoomCount;
+            return new Room(id,"000",0,0,0);
+        }
+         
+    }
+}
